be72e05bf843e257e188294b61c2c1ff6c4ccb72
Azrotho <azrotho.yt@gmail.com>
2023-10-23 15:58:26 +0200

    cooldown manager

--- src/fr/azrotho/halloweenduo/command/FriendCommandExecutor.java
+++ src/fr/azrotho/halloweenduo/command/FriendCommandExecutor.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 public class FriendCommandExecutor implements CommandExecutor {
 
     public static HashMap<Player, Player> duo = new HashMap<>();
+    public static HashMap<Player, Integer> cooldown = new HashMap<>();
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         return false;
